7ee78fa2ae56b78e65f00e9eb554e3c1cd025bbd
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-04-11 16:53:19 -0600

    Add tests to exhibit_spec.rb and patron_spec.rb

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+
+RSpec.describe Board do
+  describe '#initialize' do
+    it 'is an instance of Exhibit' do
+      exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      
+      expect(exhibit).to be_an_instance_of(Exhibit)
+    end
+    
+    it 'is stores attributes in a hash' do
+      exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    
+      expect(exhibit.name).to eq("Gems and Minerals")
+      expect(exhibit.cost).to eq(0)
+    end
+  end
+end
---
+++ spec/patron_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
+
+RSpec.describe Patron do
+  describe '#initialize' do
+    it 'is an instance of Patron' do
+      patron = Patron.new("Bob", 20)
+      
+      expect(patron).to be_an_instance_of(Patron)    
+    end
+    
+    it 'has attributes' do
+      patron_1 = Patron.new("Bob", 20)
+      
+      expect(patron_1.name).to eq("Bob")
+      expect(patron_1.spending_money).to eq(20)
+      expect(patron_1.interests).to eq([])
+    end
+    
+    it 'can add interests' do
+      patron_1 = Patron.new("Bob", 20)
+      
+      expect(patron_1.interests).to eq([])
+
+      patron_1.add_interest("Gems and Minerals")
+      patron_1.add_interest("Dead Sea Scrolls")
+
+      expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+    end
+  end
+end
